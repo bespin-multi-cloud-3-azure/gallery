7b524ad9533b9da72d29dc575d36a51eb37c86da
EERAH-LEE <kor.si1229@gmail.com>
2026-05-22 13:17:56 +0900

    test1

---
+++ oidc/locals.tf
@@ -0,0 +1,19 @@
+locals {
+  org       = "bespin-multi-cloud-3-azure"
+  project   = "gallery"
+  namespace = "${local.org}-${local.project}"
+
+  github_repo = "bespin-multi-cloud-3-azure/gallery"
+
+  iamrole = {
+    name       = "gha"
+    policy_arn = data.aws_iam_policy.admin_access.arn
+  }
+
+  iamoidcp = {
+    name            = "gha"
+    url             = "https://token.actions.githubusercontent.com"
+    client_id_list  = ["sts.amazonaws.com"]
+    thumbprint_list = ["ffffffffffffffffffffffffffffffffffffffff"]
+  }
+}
